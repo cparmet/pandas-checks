55cedeafc77f39f4a2f946cde56c5b484494250a
Chad Parmet <cparmet@gmail.com>
2024-06-13 08:18:51 -0400

    Organize __init__

--- pandas_vet/__init__.py
+++ pandas_vet/__init__.py
@@ -1,4 +1,20 @@
-# Select the objects to expose during a `from pandas_vet import`
+# Select what functions are included in `from pandas_vet import *`
+# i.e. don't expose DataFrameVet and SeriesVet, which aren't needed
+__all__ = [
+    "_initialize_options",
+    "describe_options",
+    "disable_checks",
+    "enable_checks",
+    "get_mode",
+    "print_time_elapsed",
+    "reset_format",
+    "set_format",
+    "set_mode",
+    "start_timer",
+]
+
+# Register our changes to the Pandas classes
+# and select functions to expose in `from pandas_vet import ...`
 from .DataFrameVet import DataFrameVet
 from .options import (
     _initialize_options,
