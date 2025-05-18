7c1e369e2e9c372c0b93e79b763f0535cecc73f6
Chad Parmet <cparmet@gmail.com>
2025-05-18 08:16:42 -0400

    Migrate from poetry to uv

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,23 @@
-from nox_poetry import Session, session
+from nox import Session, options
+from nox_uv import session
+
+options.default_venv_backend = "uv"
 
 
-@session(python=["3.8", "3.9", "3.10", "3.11"])
-def tests(session: Session) -> None:
+@session(
+    python=["3.9", "3.10", "3.11", "3.12", "3.13"],
+    uv_groups=["test"],
+)
+def test(s: Session) -> None:
     """Run the test suite."""
+    s.install(".")  # Install pandas-checks
+    s.run("pytest")
 
-    session.install(".")  # Install pandas-checks
-    session.install(
-        "pytest", "pytest-cases", "pyarrow", "openpyxl"
-    )  # Install test packages
 
-    session.run("pytest")
+# def tests(session: Session) -> None:
+
+#     # session.install(
+#     #     "pytest", "pytest-cases", "pyarrow", "openpyxl"
+#     # )  # Install test packages
+
+#     session.run("pytest")
